1a75a6951ffdcfb54bd1fa0929b5768ef54efa49
Kato Henry <henry38ernest@gmail.com>
2025-05-18 20:13:11 +0300

    initialise oop

---
+++ oop.py
@@ -0,0 +1,13 @@
+class Dog:
+    species = "Canine" # Class variable
+
+    def __init__(self, name, age):
+        self.name = name # Instance variable
+        self.age = age # Instance variable
+
+        def bark(self):
+            return f"{self.name} says Woof!"
+        
+        def get_age(self):
+            return self.age
+        
