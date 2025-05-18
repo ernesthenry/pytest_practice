96c21104aa430137059b097f1eecfa239f925e30
Kato Henry <henry38ernest@gmail.com>
2025-05-18 20:16:20 +0300

    creating objects

--- oop.py
+++ oop.py
@@ -5,9 +5,18 @@ class Dog:
         self.name = name # Instance variable
         self.age = age # Instance variable
 
-        def bark(self):
-            return f"{self.name} says Woof!"
+    def bark(self):
+        return f"{self.name} says Woof!"
+    
+    def get_age(self):
+        return self.age
         
-        def get_age(self):
-            return self.age
+dog1 = Dog("hen", 4)
+print(dog1)
+print(dog1.species) # Accessing class variable
+print(dog1.name) # Accessing instance variable
+print(dog1.age) # Accessing instance variable
+print(dog1.bark()) # Calling instance method
+print(dog1.get_age()) # Calling instance method
+
         
